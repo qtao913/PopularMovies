71a98aee20073c263028b78631314f37e7d3f7a2
qtao913 <sunny.qtao@gmail.com>
2015-12-14 22:16:59 -0800

    Use convertView to improve performance
    1.Avoiding layout inflation and object creation
    2.View holder pattern which will improve the performance of calling
    “findViewById”

--- app/src/main/java/com/example/android/popularmovies/AndroidImageAdapter.java
+++ app/src/main/java/com/example/android/popularmovies/AndroidImageAdapter.java
@@ -1,6 +1,6 @@
 package com.example.android.popularmovies;
 
-import android.content.Context;
+import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,16 +13,33 @@ import java.util.List;
  * Created by qlzh727 on 12/13/15.
  */
 public class AndroidImageAdapter extends ArrayAdapter<Movie> {
-    public AndroidImageAdapter(Context context, List<Movie> movies) {
-        super(context, 0, movies);
+    private final Activity currentActivity;
+
+    static class ViewHolder {
+        public ImageView movieItemView;
+    }
+
+    public AndroidImageAdapter(Activity currentActivity, List<Movie> movies) {
+        super(currentActivity, 0, movies);
+        this.currentActivity = currentActivity;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        View itemView = convertView;
+        if (itemView == null) {
+            LayoutInflater inflater = currentActivity.getLayoutInflater();
+            itemView = inflater.inflate(R.layout.image_item_view, null);
+            // configure view holder
+            ViewHolder viewHolder = new ViewHolder();
+            viewHolder.movieItemView = (ImageView)itemView.findViewById(R.id.movie_poster_item);
+            itemView.setTag(viewHolder);
+        }
+
+        // fill data
+        ViewHolder currentHolder = (ViewHolder)itemView.getTag();
         Movie currentMovie = getItem(position);
-        View rootView = LayoutInflater.from(getContext()).inflate(R.layout.image_item_view, parent, false);
-        ImageView movieImageView = (ImageView) rootView.findViewById(R.id.movie_poster_item);
-        movieImageView.setImageResource(currentMovie.imageResourceId);
-        return rootView;
+        currentHolder.movieItemView.setImageResource(currentMovie.imageResourceId);
+        return itemView;
     }
 }

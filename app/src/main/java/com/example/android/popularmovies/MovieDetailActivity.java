8d3347835e469bf7e9e56847d3a56cb36c1220f2
qtao913 <sunny.qtao@gmail.com>
2016-01-10 20:33:04 -0800

    Movie detailed view is improved
    1. added detailed information in the movie detailed view

--- app/src/main/java/com/example/android/popularmovies/MovieDetailActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MovieDetailActivity.java
@@ -3,21 +3,27 @@ package com.example.android.popularmovies;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.ActionBarActivity;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.StyleSpan;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.android.popularmovies.data.MovieContract;
 import com.squareup.picasso.Picasso;
+
 public class MovieDetailActivity extends ActionBarActivity {
 
     @Override
@@ -86,15 +92,46 @@ public class MovieDetailActivity extends ActionBarActivity {
 
         @Override
         public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-            ImageView movieDetailView = (ImageView) getView().findViewById(R.id.movie_detail);
             if (data.moveToFirst()) {
                 //Log.v("Count database", Integer.toString(data.getCount()));
+                TextView titleView = (TextView)getView().findViewById(R.id.movie_title);
+                String title = data.getString(
+                        data.getColumnIndex(MovieContract.MovieEntry.COLUMN_TITLE));
+                String releaseDate = data.getString(
+                        data.getColumnIndex(MovieContract.MovieEntry.COLUMN_RELEASE));
+                String year = releaseDate.split("-")[0];
+
+                SpannableStringBuilder stringBuilder = new SpannableStringBuilder(
+                        title + " (" + year + ") ");
+                stringBuilder.setSpan(new StyleSpan(Typeface.BOLD), 0, title.length(),
+                        Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+                stringBuilder.setSpan(new StyleSpan(Typeface.NORMAL), 0, year.length(),
+                        Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+                titleView.setText(stringBuilder);
+
+                // Hardcode genre and runtime at this moment
+                TextView genreView = (TextView)getView().findViewById(R.id.movie_genre);
+                genreView.setText("Test: Action | Comedy");
+
+                TextView runtimeView = (TextView)getView().findViewById(R.id.movie_runtime);
+                runtimeView.setText("Test: 120min");
+
+                TextView synopsisView = (TextView) getView().findViewById(R.id.movie_synopsis);
+                synopsisView.setText(data.getString(
+                        data.getColumnIndex(MovieContract.MovieEntry.COLUMN_SYNOPSIS)));
+                
+                ImageView posterView = (ImageView) getView().findViewById(R.id.movie_poster);
                 int index = data.getColumnIndex(MovieContract.MovieEntry.COLUMN_IMAGE_URL);
                 if (data.getString(index) == null) {
-                    Picasso.with(getActivity()).load(R.drawable.image_place_holder).resize(185, 252).into(movieDetailView);
+                    Picasso.with(getActivity()).load(R.drawable.image_place_holder).resize(480,640)
+                    .into(posterView);
                 } else {
-                    Picasso.with(getActivity()).load(data.getString(index)).into(movieDetailView);
+                    Picasso.with(getActivity()).load(data.getString(index)).into(posterView);
                 }
+
+                TextView ratingView = (TextView) getView().findViewById(R.id.movie_rating);
+                ratingView.setText(Double.toString(data.getDouble(
+                        data.getColumnIndex(MovieContract.MovieEntry.COLUMN_RATING))));
             }
         }
 
